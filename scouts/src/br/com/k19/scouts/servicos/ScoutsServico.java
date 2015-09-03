ec5cb3b81eab5f0b099e56ea050540fe4ca06825
cosen <rcosen@gmail.com>
2015-09-03 17:48:00 -0300

    volta do algoritmo de sorteio

--- scouts/src/br/com/k19/scouts/servicos/ScoutsServico.java
+++ scouts/src/br/com/k19/scouts/servicos/ScoutsServico.java
@@ -137,7 +137,7 @@ public class ScoutsServico {
 
 		// pontuação dos jogadores que estarão no sorteio
 		Calendar inicio = Calendar.getInstance();
-		inicio.add(Calendar.MONTH, -3);
+		inicio.add(Calendar.MONTH, -2);
 
 		List<Pontuacao> classificacaoGeral = this.geraClassificacao(inicio,
 				Calendar.getInstance());
@@ -171,130 +171,130 @@ public class ScoutsServico {
 			}
 		}
 
-		/* POTE 0 */
-		Collections.shuffle(potes.get(0));
-		for (int i = 0; i < numeroDeTimes; i++) {
-			if (potes.get(0).size() == 0) {
-				break;
-			}
-
-			Jogador x = potes.get(0).remove(0);
-			times.get(i).getJogadores().add(x);
-		}
-		
-		/* POTE 1 */
-		for (int i = 0; i < numeroDeTimes; i++) {
-			if (potes.get(1).size() == 0) {
-				break;
-			}
-			
-			int[] contadorDeJogos = new int[potes.get(1).size()];
-			for (int j = 0; j < contadorDeJogos.length; j++) {
-				Jogador jogador0 = potes.get(1).get(j);
-				Jogador jogador1 = times.get(i).getJogadores().get(0);
-				contadorDeJogos[j] = this.timeRepositorio.contaTimesPorDupla(jogador0, jogador1);
-			}
-			
-			int indiceMenor = 0;
-			for (int j = 1; j < contadorDeJogos.length; j++) {
-				if(contadorDeJogos[indiceMenor] > contadorDeJogos[j]) {
-					indiceMenor = j;
-				}
-			}
-			
-			Jogador x = potes.get(1).remove(indiceMenor);
-			times.get(i).getJogadores().add(x);
-		}
-		
-		/* POTE 2 */
-		for (int i = 0; i < numeroDeTimes; i++) {
-			if (potes.get(2).size() == 0) {
-				break;
-			}
-			
-			int[] contadorDeJogos = new int[potes.get(2).size()];
-			for (int j = 0; j < contadorDeJogos.length; j++) {
-				Jogador jogador0 = potes.get(2).get(j);
-				Jogador jogador1 = times.get(i).getJogadores().get(0);
-				Jogador jogador2 = times.get(i).getJogadores().get(1);
-				contadorDeJogos[j] = this.timeRepositorio.contaTimesPorDupla(jogador0, jogador1, jogador2);
-			}
-			
-			int indiceMenor = 0;
-			for (int j = 1; j < contadorDeJogos.length; j++) {
-				if(contadorDeJogos[indiceMenor] > contadorDeJogos[j]) {
-					indiceMenor = j;
-				}
-			}
-			
-			Jogador x = potes.get(2).remove(indiceMenor);
-			times.get(i).getJogadores().add(x);
-		}
-		
-		/* POTE 3 */
-		for (int i = 0; i < numeroDeTimes; i++) {
-			if (potes.get(3).size() == 0) {
-				break;
-			}
-			
-			int[] contadorDeJogos = new int[potes.get(3).size()];
-			for (int j = 0; j < contadorDeJogos.length; j++) {
-				Jogador jogador0 = potes.get(3).get(j);
-				Jogador jogador1 = times.get(i).getJogadores().get(0);
-				Jogador jogador2 = times.get(i).getJogadores().get(1);
-				Jogador jogador3 = times.get(i).getJogadores().get(2);
-				contadorDeJogos[j] = this.timeRepositorio.contaTimesPorDupla(jogador0, jogador1, jogador2, jogador3);
-			}
-			
-			int indiceMenor = 0;
-			for (int j = 1; j < contadorDeJogos.length; j++) {
-				if(contadorDeJogos[indiceMenor] > contadorDeJogos[j]) {
-					indiceMenor = j;
-				}
-			}
-			
-			Jogador x = potes.get(3).remove(indiceMenor);
-			times.get(i).getJogadores().add(x);
-		}
-		
-		/* POTE 4 */
-		for (int i = 0; i < numeroDeTimes; i++) {
-			if (potes.get(4).size() == 0) {
-				break;
-			}
-			
-			int[] contadorDeJogos = new int[potes.get(4).size()];
-			for (int j = 0; j < contadorDeJogos.length; j++) {
-				Jogador jogador0 = potes.get(4).get(j);
-				Jogador jogador1 = times.get(i).getJogadores().get(0);
-				Jogador jogador2 = times.get(i).getJogadores().get(1);
-				Jogador jogador3 = times.get(i).getJogadores().get(2);
-				Jogador jogador4 = times.get(i).getJogadores().get(3);
-				contadorDeJogos[j] = this.timeRepositorio.contaTimesPorDupla(jogador0, jogador1, jogador2, jogador3, jogador4);
-			}
-			
-			int indiceMenor = 0;
-			for (int j = 1; j < contadorDeJogos.length; j++) {
-				if(contadorDeJogos[indiceMenor] > contadorDeJogos[j]) {
-					indiceMenor = j;
-				}
-			}
-			
-			Jogador x = potes.get(4).remove(indiceMenor);
-			times.get(i).getJogadores().add(x);
-		}
+//		/* POTE 0 */
+//		Collections.shuffle(potes.get(0));
+//		for (int i = 0; i < numeroDeTimes; i++) {
+//			if (potes.get(0).size() == 0) {
+//				break;
+//			}
+//
+//			Jogador x = potes.get(0).remove(0);
+//			times.get(i).getJogadores().add(x);
+//		}
+//		
+//		/* POTE 1 */
+//		for (int i = 0; i < numeroDeTimes; i++) {
+//			if (potes.get(1).size() == 0) {
+//				break;
+//			}
+//			
+//			int[] contadorDeJogos = new int[potes.get(1).size()];
+//			for (int j = 0; j < contadorDeJogos.length; j++) {
+//				Jogador jogador0 = potes.get(1).get(j);
+//				Jogador jogador1 = times.get(i).getJogadores().get(0);
+//				contadorDeJogos[j] = this.timeRepositorio.contaTimesPorDupla(jogador0, jogador1);
+//			}
+//			
+//			int indiceMenor = 0;
+//			for (int j = 1; j < contadorDeJogos.length; j++) {
+//				if(contadorDeJogos[indiceMenor] > contadorDeJogos[j]) {
+//					indiceMenor = j;
+//				}
+//			}
+//			
+//			Jogador x = potes.get(1).remove(indiceMenor);
+//			times.get(i).getJogadores().add(x);
+//		}
+//		
+//		/* POTE 2 */
+//		for (int i = 0; i < numeroDeTimes; i++) {
+//			if (potes.get(2).size() == 0) {
+//				break;
+//			}
+//			
+//			int[] contadorDeJogos = new int[potes.get(2).size()];
+//			for (int j = 0; j < contadorDeJogos.length; j++) {
+//				Jogador jogador0 = potes.get(2).get(j);
+//				Jogador jogador1 = times.get(i).getJogadores().get(0);
+//				Jogador jogador2 = times.get(i).getJogadores().get(1);
+//				contadorDeJogos[j] = this.timeRepositorio.contaTimesPorDupla(jogador0, jogador1, jogador2);
+//			}
+//			
+//			int indiceMenor = 0;
+//			for (int j = 1; j < contadorDeJogos.length; j++) {
+//				if(contadorDeJogos[indiceMenor] > contadorDeJogos[j]) {
+//					indiceMenor = j;
+//				}
+//			}
+//			
+//			Jogador x = potes.get(2).remove(indiceMenor);
+//			times.get(i).getJogadores().add(x);
+//		}
+//		
+//		/* POTE 3 */
+//		for (int i = 0; i < numeroDeTimes; i++) {
+//			if (potes.get(3).size() == 0) {
+//				break;
+//			}
+//			
+//			int[] contadorDeJogos = new int[potes.get(3).size()];
+//			for (int j = 0; j < contadorDeJogos.length; j++) {
+//				Jogador jogador0 = potes.get(3).get(j);
+//				Jogador jogador1 = times.get(i).getJogadores().get(0);
+//				Jogador jogador2 = times.get(i).getJogadores().get(1);
+//				Jogador jogador3 = times.get(i).getJogadores().get(2);
+//				contadorDeJogos[j] = this.timeRepositorio.contaTimesPorDupla(jogador0, jogador1, jogador2, jogador3);
+//			}
+//			
+//			int indiceMenor = 0;
+//			for (int j = 1; j < contadorDeJogos.length; j++) {
+//				if(contadorDeJogos[indiceMenor] > contadorDeJogos[j]) {
+//					indiceMenor = j;
+//				}
+//			}
+//			
+//			Jogador x = potes.get(3).remove(indiceMenor);
+//			times.get(i).getJogadores().add(x);
+//		}
+//		
+//		/* POTE 4 */
+//		for (int i = 0; i < numeroDeTimes; i++) {
+//			if (potes.get(4).size() == 0) {
+//				break;
+//			}
+//			
+//			int[] contadorDeJogos = new int[potes.get(4).size()];
+//			for (int j = 0; j < contadorDeJogos.length; j++) {
+//				Jogador jogador0 = potes.get(4).get(j);
+//				Jogador jogador1 = times.get(i).getJogadores().get(0);
+//				Jogador jogador2 = times.get(i).getJogadores().get(1);
+//				Jogador jogador3 = times.get(i).getJogadores().get(2);
+//				Jogador jogador4 = times.get(i).getJogadores().get(3);
+//				contadorDeJogos[j] = this.timeRepositorio.contaTimesPorDupla(jogador0, jogador1, jogador2, jogador3, jogador4);
+//			}
+//			
+//			int indiceMenor = 0;
+//			for (int j = 1; j < contadorDeJogos.length; j++) {
+//				if(contadorDeJogos[indiceMenor] > contadorDeJogos[j]) {
+//					indiceMenor = j;
+//				}
+//			}
+//			
+//			Jogador x = potes.get(4).remove(indiceMenor);
+//			times.get(i).getJogadores().add(x);
+//		}
 		
 		// espalhando os jogadores
-//		for (int i = 0; i < numeroDeTimes; i++) {
-//			for (int j = 0; j < 5; j++) {
-//				if (potes.get(j).size() == 0) {
-//					break;
-//				}
-//				Collections.shuffle(potes.get(j));
-//				Jogador x = potes.get(j).remove(0);
-//				times.get(i).getJogadores().add(x);
-//			}
-//		}
+		for (int i = 0; i < numeroDeTimes; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (potes.get(j).size() == 0) {
+					break;
+				}
+				Collections.shuffle(potes.get(j));
+				Jogador x = potes.get(j).remove(0);
+				times.get(i).getJogadores().add(x);
+			}
+		}
 
 		// salvando os times
 		for (Time x : times) {
